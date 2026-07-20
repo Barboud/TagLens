159e364269e14859dc586093a7d2f44ca63067c4
Salem Ba-Rabuod <barboud@gmail.com>
2026-07-20 20:58:04 +0200

    initialize

---
+++ src/main/java/dev/barboud/TagLens/TagLensApplication.java
@@ -0,0 +1,13 @@
+package dev.barboud.TagLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagLensApplication.class, args);
+	}
+
+}
